f3b317897f905ba3dd1203f730d8d717a4752e62
Adam Page <adam@adampage.net>
2022-08-06 18:51:09 -0700

    Add capability for surplus (ref #2)

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,13 +1,22 @@
 const { widget } = figma
 const { AutoLayout, SVG, Text, Rectangle, useSyncedState, usePropertyMenu } = widget
 
-const minCount = 0
 const initCount = 0
-const minTarget = 0
+const minTarget = 1
 const initTarget = 5
 const widthWidget = 240
 const paddingWidget = 20
 const heightProgressBar = 24
+const heightSurplusBar = heightProgressBar - 4
+const fontSizeTarget = 12
+const fontSizeSurplus = 10
+const paddingHorizontalTarget = 8
+const paddingHorizontalSurplus = 4
+const widthTargetEmoji = fontSizeTarget
+const widthTargetSpace = fontSizeTarget * (1 / 5)
+const widthTargetDigit = fontSizeTarget * (3 / 4)
+const widthSurplusSpace = fontSizeSurplus * (1 / 5)
+const widthSurplusDigit = fontSizeSurplus * (3 / 4)
 
 const optionsColorProgressBar = [
   { option: "#5963FF", tooltip: "Blue" },
@@ -16,30 +25,19 @@ const optionsColorProgressBar = [
   { option: "#EB6100", tooltip: "Orange" },
   { option: "#8AAF3E", tooltip: "Green" },
   { option: "#FF545E", tooltip: "Red" },
-  { option: "#CA9000", tooltip: "Gold" }
+  { option: "#CA9000", tooltip: "Gold" },
+  { option: "#444444", tooltip: "Charcoal" }
 ]
 
 const colorDefault = optionsColorProgressBar[0].option
 
-const srcButtonInc = `
-<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <rect width="30" height="30" rx="4" fill="#111"/>
-  <path d="M15.9375 7.5H14.0625V14.0625H7.5V15.9375H14.0625V22.5H15.9375V15.9375H22.5V14.0625H15.9375V7.5Z" fill="#fff" fill-opacity="1"/>
-</svg>
-`
-
-const srcButtonDec = `
-<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <rect width="30" height="30" rx="4" fill="#111"/>
-  <rect x="7.5" y="14.0625" width="15" height="1.875" fill="#fff" fill-opacity="1"/>
-</svg>
-`
-
 function ProgressBar() {
+  const propertyMenu: WidgetPropertyMenuItem[] = []
   const [colorProgressBar, setColorProgressBar] = useSyncedState("colorProgressBar", colorDefault)
   const [numCount, setCount] = useSyncedState("count", initCount)
   const [numTarget, setTarget] = useSyncedState("target", initTarget)
-  const propertyMenu: WidgetPropertyMenuItem[] = []
+  const numSurplus = Math.max(0, numCount - numTarget)
+  const metTarget = numCount >= numTarget
 
   propertyMenu.push({
     tooltip: 'Target - 1',
@@ -74,80 +72,182 @@ function ProgressBar() {
   usePropertyMenu(propertyMenu, ({ propertyName, propertyValue }) => {
     if (propertyName === 'colorProgressBar' && propertyValue) {
       setColorProgressBar(propertyValue)
-    } else if (propertyName === 'incProgress' && numCount < numTarget) {
-      setCount(numCount + 1)
-    } else if (propertyName === 'decProgress' && numCount > initCount) {
-      setCount(numCount - 1)
     } else if (propertyName === 'incTarget') {
       setTarget(numTarget + 1)
     } else if (propertyName === 'decTarget' && numTarget > minTarget) {
       setTarget(numTarget - 1)
-      if (numCount >= numTarget) {
-        setCount(numTarget - 1)
-      }
     } else if (propertyName === 'reset') {
       setCount(initCount)
       setTarget(initTarget)
     }
   })
 
-  const widthContainer = widthWidget - (paddingWidget * 2)
-  const widthProgressBar = (numCount / numTarget) * widthContainer
+  // Caluclate progress and overflow widths
+  const widthAvailableSpace = widthWidget - (paddingWidget * 2)
+  const widthProgressSpace = !numSurplus ? widthAvailableSpace : (numTarget / numCount) * widthAvailableSpace
+  const widthProgressBar = (numCount / numTarget) * widthProgressSpace
+  const widthOverflowArea = Math.max(0.01, widthAvailableSpace - widthProgressSpace)
+  const widthSurplusArea = Math.max(0.01, widthOverflowArea - 1)
+
+  // Prepare text summaries and determine layout triggers
+  const textProgress = metTarget ? `👍 ${numTarget}` : `${Math.min(numCount, numTarget)} of ${numTarget}`
+  const textSurplus = numSurplus ? ` + ${numSurplus}` : ``
+  const numDigitsTarget = numTarget.toString().length
+  const numDigitsSurplus = numSurplus.toString().length
+  const widthTextTarget = (paddingHorizontalTarget * 2) + widthTargetEmoji + widthTargetSpace + (numDigitsTarget * widthTargetDigit)
+  const widthTextSurplus = (paddingHorizontalSurplus * 2) + widthSurplusSpace + widthSurplusDigit + widthSurplusSpace + (numDigitsSurplus * widthSurplusDigit)
+  const targetCanFitText = widthProgressSpace > widthTextTarget
+  const overflowCanFitText = widthOverflowArea > widthTextSurplus
 
   return (
     <AutoLayout
-      direction="vertical"
+      name="Widget"
+      direction="horizontal"
       verticalAlignItems="center"
       width={widthWidget}
       padding={paddingWidget}
     >
+
       <AutoLayout
+        name="Available space"
+        direction="horizontal"
+        verticalAlignItems="center"
         width="fill-parent"
-        height={heightProgressBar}
-        fill="#fff"
-        stroke="#666"
-        strokeAlign="inside"
-        cornerRadius={2}
       >
         <AutoLayout
-          positioning="absolute"
-          x={0}
-          y={0}
-          width={widthContainer}
+          name="Target space"
+          width={widthProgressSpace}
           height={heightProgressBar}
+          fill="#fff"
+          stroke={{ r: 0, g: 0, b: 0, a: 0.75 }}
+          strokeWidth={1}
+          strokeAlign="inside"
+          cornerRadius={2}
         >
           <AutoLayout
-            verticalAlignItems="center"
-            height="fill-parent"
-            padding={8}
+            name="Progress space"
+            positioning="absolute"
+            x={0}
+            y={0}
+            width={widthProgressSpace}
+            height={heightProgressBar}
           >
-            <Text fontSize={12} fontWeight={900} fill="#666">
-              {numCount} of {numTarget}
-            </Text>
+            <AutoLayout
+              name="]-["
+              verticalAlignItems="center"
+              height="fill-parent"
+              padding={{ horizontal: paddingHorizontalTarget, vertical: 0 }}
+            >
+              <Text
+                name="Progress text / Dark"
+                fontSize={fontSizeTarget}
+                fontWeight={900}
+                fill={{ r: 0, g: 0, b: 0, a: 0.75 }}
+              >
+                {textProgress}
+              </Text>
+            </AutoLayout>
+          </AutoLayout>
+          <AutoLayout
+            name="Progress bar"
+            hidden={!numCount}
+            positioning="absolute"
+            x={0}
+            y={0}
+            width={widthProgressBar || 0.01}
+            height={heightProgressBar}
+            fill={numCount < numTarget ? colorProgressBar : colorProgressBar}
+          >
+            <AutoLayout
+              name="]-["
+              verticalAlignItems="center"
+              width={widthProgressBar || 0.01}
+              height="fill-parent"
+              padding={{ horizontal: paddingHorizontalTarget, vertical: 0 }}
+              fill={metTarget ? { r: 0, g: 0, b: 0, a: 0.32 } : { r: 0, g: 0, b: 0, a: 0 }}
+            >
+              <Text
+                name="Progress text / Light"
+                hidden={!targetCanFitText}
+                fontSize={fontSizeTarget}
+                fontWeight={900}
+                fill="#fff"
+              >
+                {textProgress}
+              </Text>
+            </AutoLayout>
           </AutoLayout>
         </AutoLayout>
         <AutoLayout
-          positioning="absolute"
-          x={0}
-          y={0}
-          width={widthProgressBar || 0.01}
-          height={heightProgressBar}
-          fill={numCount < numTarget ? colorProgressBar : colorProgressBar}
+          name="Overflow space"
+          hidden={!numSurplus}
+          width={widthOverflowArea}
+          height={heightSurplusBar}
         >
           <AutoLayout
-            verticalAlignItems="center"
-            height="fill-parent"
-            padding={8}
+            name="Surplus space"
+            positioning="absolute"
+            x={1}
+            y={0}
+            width={widthSurplusArea}
+            height={heightSurplusBar}
           >
-            <Text fontSize={12} fontWeight={900} fill="#fff">
-              {numCount} of {numTarget}
-            </Text>
+            <AutoLayout
+              name="]-["
+              verticalAlignItems="center"
+              height="fill-parent"
+              padding={{ horizontal: paddingHorizontalSurplus, vertical: 0 }}
+            >
+            </AutoLayout>
+          </AutoLayout>
+          <AutoLayout
+            name="Surplus bar"
+            positioning="absolute"
+            x={1}
+            y={0}
+            width={widthSurplusArea}
+            height={heightSurplusBar}
+            fill={numCount < numTarget ? colorProgressBar : colorProgressBar}
+            stroke={{ r: 0, g: 0, b: 0, a: 0.2 }}
+            strokeWidth={1}
+            strokeAlign="inside"
+            cornerRadius={{ topRight: 2, bottomRight: 2 }}
+          >
           </AutoLayout>
         </AutoLayout>
+        <AutoLayout
+          name="Surplus text"
+          positioning="absolute"
+          horizontalAlignItems={overflowCanFitText ? "start" : "end"}
+          verticalAlignItems="center"
+          x={overflowCanFitText ? widthProgressSpace : 0}
+          y={0}
+          width={overflowCanFitText ? widthOverflowArea : widthProgressSpace}
+          height={heightProgressBar}
+          padding={overflowCanFitText ? { horizontal: paddingHorizontalSurplus } : { horizontal: paddingHorizontalTarget }}
+        >
+          <Text
+            name="Progress text / Light"
+            hidden={targetCanFitText}
+            fontSize={fontSizeTarget}
+            fontWeight={900}
+            fill="#fff"
+          >
+            {textProgress}
+          </Text>
+          <Text
+            name="Surplus text / Light"
+            fontSize={fontSizeSurplus}
+            fontWeight={700}
+            fill="#fff"
+          >
+            {textSurplus}
+          </Text>
+        </AutoLayout>
       </AutoLayout>
-
-
       <AutoLayout
+        name="Button / Decrement"
+        tooltip="Reduce progress by 1"
         positioning="absolute"
         x={0}
         y={paddingWidget}
@@ -175,6 +275,8 @@ function ProgressBar() {
         </Text>
       </AutoLayout>
       <AutoLayout
+        name="Button / Increment"
+        tooltip="Increase progress by 1"
         positioning="absolute"
         x={widthWidget / 2}
         y={paddingWidget}
@@ -187,9 +289,7 @@ function ProgressBar() {
           opacity: 1
         }}
         onClick={() => {
-          if (numCount < numTarget) {
-            setCount(numCount + 1)
-          }
+          setCount(numCount + 1)
         }}
       >
         <Text
